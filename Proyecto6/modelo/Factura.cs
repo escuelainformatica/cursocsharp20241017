5a12dcf835c2525b2128d944c70c78c739a04cfc
jorgecc <jcastro@eftec.cl>
2024-10-17 15:21:47 -0300

    primero

---
+++ Proyecto6/modelo/Factura.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto6.modelo
+{
+    public class Factura
+    {
+        // propiedades
+        public int NumFactura { set; get; }
+        public string NombreCliente { set; get; }
+        public string Fecha { set; get; }
+        public int Total { set; get; }
+        // con la ampollea (con el boton derecho encima de la clase -> constructor)
+        public Factura(int numFactura, string nombreCliente, string fecha, int total)
+        {
+            NumFactura = numFactura;
+            NombreCliente = nombreCliente;
+            Fecha = fecha;
+            Total = total;
+        }
+
+        public Factura()
+        {
+        }
+
+
+ 
+
+
+
+    }
+}
---
+++ Proyecto6/servicio/FacturaServicio.cs
@@ -0,0 +1,35 @@
+﻿using Proyecto6.modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto6.servicio
+{
+    public static class FacturaServicio
+    {
+        // metodo 
+        // public <el valor que se va a devolver> <nombre la funcion>(<los argumentos>)
+        // Factura --> funcion -> nada (void)
+        public static void MostrarFactura(Factura fact)
+        {
+            Console.WriteLine("----------fact:---------------");
+            Console.WriteLine(fact.NumFactura);
+            Console.WriteLine(fact.NombreCliente);
+            Console.WriteLine(fact.Fecha);
+            Console.WriteLine(fact.Total);
+        }
+        // una funcion que devuelve el total de la factura con el IVA.
+        public static double TotalFactura(Factura fact)
+        {
+            return fact.Total * 1.19;
+        }
+        // una funcion que suma dos facturas y devuelve el total
+        public static int SumarFactura(Factura fact1,Factura fact2)
+        {
+            return fact1.Total + fact2.Total;
+        }
+
+    }
+}
